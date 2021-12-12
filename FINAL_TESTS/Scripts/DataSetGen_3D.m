78b3d7de00074368bfebe156c19f1adcca71d993
Abhinav Gupta <42617586+abhinavG37@users.noreply.github.com>
2021-12-12 18:47:29 -0500

    KNN 3d Script generated, needs evaluation

--- FINAL_TESTS/Scripts/DataSetGen_3D.m
+++ FINAL_TESTS/Scripts/DataSetGen_3D.m
@@ -11,10 +11,8 @@ z = 0:GridResolution : ZMax;
 
 [X,Y,Z] = meshgrid(x,y,z);
 
-
-
 %% Define Antenna Positions  X= Index (1) of grid Y= Index(2) of grid Z = index(3)
-AntennaPosIndex = [X(1), Y(1), Z(1) ; 
+AntennaPosIndex = [X(1), Y(1), Z(1) ; X(1), Y(1), Z(end); X(1), Y(end), Z(end); 
                                     X(end),Y(end),  Z(end)]; % [m]
 
 
@@ -63,11 +61,30 @@ Data.Antenna2.DistMat = zeros( XMax / GridResolution, YMax / GridResolution,ZMax
 Data.Antenna3.DistMat = zeros( XMax / GridResolution, YMax / GridResolution,ZMax/GridResolution);
 Data.Antenna4.DistMat = zeros( XMax / GridResolution, YMax / GridResolution,ZMax/GridResolution);
 
+%% Uniformly distribute along the entire room
+for AntennaPosCounter = 1:size(AntennaPosIndex,1)
+   for x_index = 1 : (XMax) / GridResolution +1
+      for y_index =1 : (YMax) / GridResolution +1
+         for z_index =1 : (ZMax) / GridResolution +1
+            Tag_pos = [(x_index-1)*GridResolution, (y_index-1)*GridResolution, (z_index-1)*GridResolution];
+          
+              if (x_index>0 && x_index<XMax && y_index >0 && y_index<YMax && z_index>0 && z_index<ZMax)
+                        Data.Antenna1.DistMat( x_index,y_index,z_index)=   nan;
+                        Data.Antenna2.DistMat( x_index,y_index,z_index)=   nan;
+                        Data.Antenna3.DistMat( x_index,y_index,z_index)=   nan;
+                        Data.Antenna4.DistMat( x_index,y_index,z_index)=   nan;
+                else
+                        Data.Antenna1.DistMat( x_index,y_index,z_index)=  distanceCalc(Data.Antenna1.AntennaPos, Tag_pos);
+                        Data.Antenna2.DistMat( x_index,y_index,z_index)=  distanceCalc(Data.Antenna2.AntennaPos, Tag_pos);
+                        Data.Antenna3.DistMat( x_index,y_index,z_index)=  distanceCalc(Data.Antenna3.AntennaPos, Tag_pos);
+                        Data.Antenna4.DistMat( x_index,y_index,z_index)=  distanceCalc(Data.Antenna4.AntennaPos, Tag_pos);
+              end
+            end
+        end
+    end
+end
 
-
-
-
-
+clearvars -except Data DistMatrix GridResolution XMax YMax ZMax
 
 function [dist3d] = distanceCalc(AntennaPos, TagPos)
     dist3d = norm(TagPos-AntennaPos);
