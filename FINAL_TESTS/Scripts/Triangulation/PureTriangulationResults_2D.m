899daa8a79af5dbe9e32d582112f081bc93876f7
Abhinav Gupta <abhinavgupta1998@gmail.com>
2021-12-16 15:17:20 -0500

    Changed Model name used in script

--- FINAL_TESTS/Scripts/Triangulation/PureTriangulationResults_2D.m
+++ FINAL_TESTS/Scripts/Triangulation/PureTriangulationResults_2D.m
@@ -18,7 +18,7 @@ TagLocations = [3.5,1;3,2;4,2;2,3];
 
 %% Attaching distance Dataset to simulation
 
-model = 'KNN_DataGen';
+model = 'antenna_sim';
 open(model)
   % ====Simulation Params Setup===== %
             FreqCarrier = 9.15e+08;
@@ -35,47 +35,47 @@ open(model)
             R4 = 2.236;
             Rtest = 1;
 
-            set_param('KNN_DataGen/Antenna_TX','Gr',num2str(Gt))
-            set_param('KNN_DataGen/Antenna_TX','Zin',num2str(Zin_t))
-            set_param('KNN_DataGen/Antenna_TX','CarrierFreqRad',num2str(FreqCarrier))
-            set_param('KNN_DataGen/PA','Zin',num2str(Zin_r))      
-            set_param('KNN_DataGen/Inport','CarrierFreq',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_TX','Gr',num2str(Gt))
+            set_param('antenna_sim/Antenna_TX','Zin',num2str(Zin_t))
+            set_param('antenna_sim/Antenna_TX','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/PA','Zin',num2str(Zin_r))      
+            set_param('antenna_sim/Inport','CarrierFreq',num2str(FreqCarrier))
             
-            set_param('KNN_DataGen/Antenna_RX','Gr',num2str(Gr))
-            set_param('KNN_DataGen/Antenna_RX1','Gr',num2str(Gr))
-            set_param('KNN_DataGen/Antenna_RX2','Gr',num2str(Gr))
-            set_param('KNN_DataGen/Antenna_RX3','Gr',num2str(Gr))
-            set_param('KNN_DataGen/Antenna_RX4','Gr',num2str(Gr))
-            set_param('KNN_DataGen/Antenna_RX','Zin',num2str(Zin_r))
-            set_param('KNN_DataGen/Antenna_RX1','Zin',num2str(Zin_r))
-            set_param('KNN_DataGen/Antenna_RX2','Zin',num2str(Zin_r))
-            set_param('KNN_DataGen/Antenna_RX3','Zin',num2str(Zin_r))
-            set_param('KNN_DataGen/Antenna_RX4','Zin',num2str(Zin_r))
-            set_param('KNN_DataGen/Antenna_RX','CarrierFreqRad',num2str(FreqCarrier))
-            set_param('KNN_DataGen/Antenna_RX1','CarrierFreqRad',num2str(FreqCarrier))
-            set_param('KNN_DataGen/Antenna_RX2','CarrierFreqRad',num2str(FreqCarrier))
-            set_param('KNN_DataGen/Antenna_RX3','CarrierFreqRad',num2str(FreqCarrier))
-            set_param('KNN_DataGen/Antenna_RX4','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_RX','Gr',num2str(Gr))
+            set_param('antenna_sim/Antenna_RX1','Gr',num2str(Gr))
+            set_param('antenna_sim/Antenna_RX2','Gr',num2str(Gr))
+            set_param('antenna_sim/Antenna_RX3','Gr',num2str(Gr))
+            set_param('antenna_sim/Antenna_RX4','Gr',num2str(Gr))
+            set_param('antenna_sim/Antenna_RX','Zin',num2str(Zin_r))
+            set_param('antenna_sim/Antenna_RX1','Zin',num2str(Zin_r))
+            set_param('antenna_sim/Antenna_RX2','Zin',num2str(Zin_r))
+            set_param('antenna_sim/Antenna_RX3','Zin',num2str(Zin_r))
+            set_param('antenna_sim/Antenna_RX4','Zin',num2str(Zin_r))
+            set_param('antenna_sim/Antenna_RX','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_RX1','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_RX2','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_RX3','CarrierFreqRad',num2str(FreqCarrier))
+            set_param('antenna_sim/Antenna_RX4','CarrierFreqRad',num2str(FreqCarrier))
              
              
-            set_param('KNN_DataGen/FS_PathLoss1','Gain',num2str(lambdaCarrier/(4*pi*R1)))
-            set_param('KNN_DataGen/FS_PathLoss2','Gain',num2str(lambdaCarrier/(4*pi*R2)))
-            set_param('KNN_DataGen/FS_PathLoss3','Gain',num2str(lambdaCarrier/(4*pi*R3)))
-            set_param('KNN_DataGen/FS_PathLoss4','Gain',num2str(lambdaCarrier/(4*pi*R4)))
-            set_param('KNN_DataGen/FS_PathLossTest','Gain',num2str(lambdaCarrier/(4*pi*Rtest)))
+            set_param('antenna_sim/FS_PathLoss1','Gain',num2str(lambdaCarrier/(4*pi*R1)))
+            set_param('antenna_sim/FS_PathLoss2','Gain',num2str(lambdaCarrier/(4*pi*R2)))
+            set_param('antenna_sim/FS_PathLoss3','Gain',num2str(lambdaCarrier/(4*pi*R3)))
+            set_param('antenna_sim/FS_PathLoss4','Gain',num2str(lambdaCarrier/(4*pi*R4)))
+            set_param('antenna_sim/FS_PathLossTest','Gain',num2str(lambdaCarrier/(4*pi*Rtest)))
               
-            set_param('KNN_DataGen/LNA','Zin',num2str(Zin_r'))
-            set_param('KNN_DataGen/LNA1','Zin',num2str(Zin_r'))
-            set_param('KNN_DataGen/LNA2','Zin',num2str(Zin_r'))
-            set_param('KNN_DataGen/LNA3','Zin',num2str(Zin_r'))
-            set_param('KNN_DataGen/LNA4','Zin',num2str(Zin_r'))
+            set_param('antenna_sim/LNA','Zin',num2str(Zin_r'))
+            set_param('antenna_sim/LNA1','Zin',num2str(Zin_r'))
+            set_param('antenna_sim/LNA2','Zin',num2str(Zin_r'))
+            set_param('antenna_sim/LNA3','Zin',num2str(Zin_r'))
+            set_param('antenna_sim/LNA4','Zin',num2str(Zin_r'))
              
      
-           set_param('KNN_DataGen/RA1','CarrierFreq',num2str(FreqCarrier))
-           set_param('KNN_DataGen/RA2','CarrierFreq',num2str(FreqCarrier))
-           set_param('KNN_DataGen/RA3','CarrierFreq',num2str(FreqCarrier))
-           set_param('KNN_DataGen/RA4','CarrierFreq',num2str(FreqCarrier))
-           set_param('KNN_DataGen/RA5','CarrierFreq',num2str(FreqCarrier))
+           set_param('antenna_sim/RA1','CarrierFreq',num2str(FreqCarrier))
+           set_param('antenna_sim/RA2','CarrierFreq',num2str(FreqCarrier))
+           set_param('antenna_sim/RA3','CarrierFreq',num2str(FreqCarrier))
+           set_param('antenna_sim/RA4','CarrierFreq',num2str(FreqCarrier))
+           set_param('antenna_sim/RA5','CarrierFreq',num2str(FreqCarrier))
 
             
 %% Coordinate System on Matrices
@@ -120,10 +120,10 @@ Data.Antenna4.RSSI = zeros(4,1);
         %============SIMULATION CALL==================%
         
 
-        set_param('KNN_DataGen/FS_PathLoss1','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(1,:), TagLocations(TagLocationsCounter,:)))));
-        set_param('KNN_DataGen/FS_PathLoss2','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(2,:), TagLocations(TagLocationsCounter,:)))));
-        set_param('KNN_DataGen/FS_PathLoss3','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(3,:), TagLocations(TagLocationsCounter,:)))));
-        set_param('KNN_DataGen/FS_PathLoss4','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(4,:), TagLocations(TagLocationsCounter,:)))));
+        set_param('antenna_sim/FS_PathLoss1','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(1,:), TagLocations(TagLocationsCounter,:)))));
+        set_param('antenna_sim/FS_PathLoss2','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(2,:), TagLocations(TagLocationsCounter,:)))));
+        set_param('antenna_sim/FS_PathLoss3','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(3,:), TagLocations(TagLocationsCounter,:)))));
+        set_param('antenna_sim/FS_PathLoss4','Gain',num2str(lambdaCarrier/(4*pi*distanceCalc(AntennaPosIndex(4,:), TagLocations(TagLocationsCounter,:)))));
 
         SimOutput = sim(model, 'FastRestart', 'off');
         %Obtained Signal Strengths
